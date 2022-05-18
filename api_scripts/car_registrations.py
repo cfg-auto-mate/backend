2f28c863e57a1395e4f109e5e4700a72a6652c3d
ayana <ayan.a.k@outlook.com>
2022-05-18 04:23:58 +0100

    new car api

---
+++ api_scripts/car_registrations.py
@@ -0,0 +1,15 @@
+# code example
+
+import requests
+
+url = "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles"
+
+payload = "{\n\t\"registrationNumber\": \"AA19AAA\"\n}"
+headers = {
+  'x-api-key': 'WROj3JnkS19XuIs7qWWt99Myxf9WO4NP9EvGIDEL',
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data = payload)
+
+# print(response.text.encode('utf8'))
